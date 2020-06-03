53059365c5eb7b7895a3fea40115ecdd51325987
Julian Bustos <julian.m.bustos@gmail.com>
2020-06-03 01:08:30 +0000

    added if statements

--- timer1.js
+++ timer1.js
@@ -3,15 +3,28 @@ const c = console.log;
 
 
 const timer = (userInput) => {
-  for (let i of userInput) {
-    const time = i*1000
-    const startTime = 0; 
-    setTimeout(() => {
-      process.stdout.write('\x07');
-      console.log("it's been ", time, "seconds")
-    }, startTime + (time));
+  // console.log(userInput)
+  if (userInput.length === 0) {
+    // console.log("No input")
+    return "No input"
+  } else {
+    for (let i of userInput) {
+      i = Number(i);
+      // console.log(Number.isNaN(i))
+      // console.log(typeof i)
+      if (Number.isNaN(i)) {
+        c("We can't schedule anything " + i + " is a not a number")
+      } else if (i < 0) {
+        c("Cannot schedule anything " + i + " is a negative number")
+      } else {
+        const time = i * 1000
+        const startTime = 0;
+        setTimeout(() => {
+          process.stdout.write('\x07');
+          console.log("it's been ", i, "seconds")
+        }, startTime + (time));
+      }
+    }
   }
 }
-
-
-c(timer(userInput))
+timer(userInput)
